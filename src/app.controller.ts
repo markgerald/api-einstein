d5a213d3f25290f21bd14d11057c4b6801884524
Mark Gerald Martins <mark.g.martins@gmail.com>
2020-02-10 18:54:25 -0300

    getSum and GetTwoArrayUniqueSum methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppDto } from './app.dto';
 
@@ -8,6 +8,6 @@ export class AppController {
 
   @Post('/api/solve')
   async solve(@Body() appDto: AppDto) {
-    return this.appService.getUniqueArray(appDto.A);
+    return this.appService.getTwoArrayUniqueSum(appDto);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getUniqueArray(arrArg) {
-    return arrArg.filter(function(elem, pos,arr) {
-      return arr.indexOf(elem) == pos;
-    });
-  };
+    getUniqueArray(arrArg) {
+        return arrArg.filter(function (elem, pos, arr) {
+            return arr.indexOf(elem) == pos;
+        });
+    };
+
+    getSum(array, sum) {
+        const hashMap = {};
+        const results = [];
+        for (let i = 0; i < array.length; i++) {
+            if (hashMap[array[i]]) {
+                results.push([hashMap[array[i]], array[i]])
+            } else {
+                hashMap[sum - array[i]] = array[i];
+            }
+        }
+        return results;
+    }
+
+    getTwoArrayUniqueSum(requestBody) {
+      return (this.getSum(this.getUniqueArray(requestBody.A), requestBody.K).length > 0)? 'Yes' : 'No';
+    }
 }
